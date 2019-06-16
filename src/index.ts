4341936560123d36e6402371b22a18ad0521a5fb
Mykola.Fant <fantkolja@gmail.com>
2019-06-16 13:17:11 +0300

    update(deploy): remove static middleware

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,20 @@ import * as express from 'express';
 const port: number = Number(process.env.PORT || 5000);
 const server: ApiServer = new ApiServer(port);
 
-// TODO: move static files path to config
-const staticRoot = join(__dirname, '../../frontend/dist');
-const staticMiddleware: express.Handler = express.static(`${staticRoot}`);
-
 // app.use(logger('dev'));
 // TODO: INTERCEPT all /api calls
 // server.addControllers([TimetableController]);
 server.addControllers([]);
-server.addMiddleware(staticMiddleware);
 server.get('/', (req, res) => {
   res.status(200).json({
     version: '0.0.1',
   });
 });
 server.get('/*', (req, res) => {
-  res.status(200).sendFile(`${staticRoot}/index.html`);
+  res.status(200).json({
+    version: '0.0.1',
+    documentationURL: '/',
+  });
 });
 // TODO: useExpressServer(app, { // register created express server in routing-controllers
 //   controllers: [UserController] // and configure it the way you need (controllers, validation, etc.)
